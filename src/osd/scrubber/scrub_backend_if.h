3891cc5bfe1d5c7de7bd22fb7a3e6e0f5cd770e4
Ronen Friedman <rfriedma@redhat.com>
2021-04-11 21:17:41 +0300

    osd/scrub: extract scrub backend code into separate classes
    A refactoring of the scrub backend code (all Scrub related code
    that checks/manipulates objects' (meta)data).
    
    Part of the refactoring was breaking long chunks of code into
    separate functions. In order to avoid passing endless lists
    of state parameters from/to these new methods, the scrub backend
    state is now maintained within its own objects.
    
    a change to note:
    Instead of two scrub-map collections - one that holds all
    incoming maps, and one that holds pointers to all of those incoming
    plus the one we (the Primary) manage - just use one collection
    for both incoming and our own.
    
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>

---
+++ src/osd/scrubber/scrub_backend_if.h
@@ -0,0 +1,68 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+#pragma once
+
+#include <list>
+#include <map>
+#include <optional>
+#include <set>
+#include <utility>
+
+#include "common/scrub_types.h"
+
+struct ScrubMap;
+
+/**
+ * The scrubber's interface into its ScrubBackend object.
+ *
+ * ScrubBackend wraps the data required for the back-end part of the scrubbing:
+ * comparing the maps and fixing objects.
+ */
+class ScrubBackendIF {
+ public:
+  virtual ~ScrubBackendIF() = default;
+
+  /**
+   * reset the per-chunk data structure (ScrubberBeChunk).
+   * Create an empty scrub-map for this shard, and place it
+   * in the appropriate entry in 'received_maps'.
+   *
+   * @returns a pointer to the newly created ScrubMap.
+   */
+  virtual ScrubMap* new_chunk() = 0;
+
+  /**
+   * sets Backend's m_repair flag (setting m_mode_desc to a corresponding
+   * string)
+   */
+  virtual void update_repair_status(bool should_repair) = 0;
+
+  /**
+   * decode the arriving MOSDRepScrubMap message, placing the replica's
+   * scrub-map into received_maps[from].
+   *
+   * @param from replica
+   */
+  virtual void decode_received_map(pg_shard_t from,
+                                   const MOSDRepScrubMap& msg) = 0;
+
+  virtual void scrub_compare_maps(bool max_point_reached) = 0;
+
+  /**
+   * Go over m_authoritative - the list of "objects in distress" (i.e. missing
+   * or inconsistent) that we do have an authoritative copy of - and fix them.
+   *
+   * @return the number of objects fixed
+   */
+  virtual int scrub_process_inconsistent() = 0;
+
+  virtual void scan_snaps(ScrubMap& smap) = 0;
+
+  virtual void replica_clean_meta(ScrubMap& smap,
+                                  bool max_reached,
+                                  const hobject_t& start) = 0;
+
+  virtual int get_num_digest_updates_pending() const = 0;
+
+  virtual void repair_oinfo_oid(ScrubMap& smap) = 0;
+};
