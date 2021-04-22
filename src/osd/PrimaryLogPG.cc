107d12f0614273ddb222c602069b9a992a19ee24
Ronen Friedman <rfriedma@redhat.com>
2021-04-22 10:56:12 +0300

    osd/scrub: extracting scrub scheduling code from OSD.cc
    A separate object (ScrubQueue) now manages the collection of ScrubJob-s
    that are to be scrubbed.
    
    Notable changes:
    - ScrubQueue keeps track of the CPU load (instead of the OSD main)
    - ScrubJob-s are part of each PG's scrubber. They are registered with the
      ScrubQueue when eligible for scrubbing (i.e. - active Primary), and removed
      from it otherwise.
    - "a PG is trying to secure Replica resources" was always treated as an OSD-wide
      state that delays new scrubs from starting. Here - it is also implemented as such
      (instead of per-PG state that must be queried)
    
    Internally, the SQ maintains two groups of jobs: the regular ones, waiting to be
    scrubbed, and the "penalized" - those PGs that failed in securing their
    replicas' scrub resources (@dzafman changes, slightly modified to fit the
    new environment)
    
    Also included: minor fixes to the scrub state-machine and events priorities.
    
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>
    Co-authored-by: David Zafman <dzafman@redhat.com>

--- src/osd/PrimaryLogPG.cc
+++ src/osd/PrimaryLogPG.cc
@@ -15,43 +15,39 @@
  *
  */
 
-#include "boost/tuple/tuple.hpp"
-#include "boost/intrusive_ptr.hpp"
-#include "PG.h"
-#include "scrubber/pg_scrubber.h"
 #include "PrimaryLogPG.h"
-#include "OSD.h"
-#include "scrubber/PrimaryLogScrub.h"
-#include "OpRequest.h"
-#include "scrubber/ScrubStore.h"
-#include "Session.h"
-#include "objclass/objclass.h"
-#include "osd/ClassHandler.h"
+
+#include "boost/intrusive_ptr.hpp"
 
 #include "cls/cas/cls_cas_ops.h"
+#include "common/EventTrace.h"
 #include "common/ceph_crypto.h"
+#include "common/config.h"
 #include "common/errno.h"
-#include "common/scrub_types.h"
 #include "common/perf_counters.h"
-
-#include "messages/MOSDOp.h"
+#include "common/scrub_types.h"
+#include "include/compat.h"
+#include "messages/MCommandReply.h"
 #include "messages/MOSDBackoff.h"
-#include "messages/MOSDPGTrim.h"
-#include "messages/MOSDPGScan.h"
-#include "messages/MOSDRepScrub.h"
+#include "messages/MOSDOp.h"
 #include "messages/MOSDPGBackfill.h"
 #include "messages/MOSDPGBackfillRemove.h"
 #include "messages/MOSDPGLog.h"
+#include "messages/MOSDPGScan.h"
+#include "messages/MOSDPGTrim.h"
 #include "messages/MOSDPGUpdateLogMissing.h"
 #include "messages/MOSDPGUpdateLogMissingReply.h"
-#include "messages/MCommandReply.h"
+#include "messages/MOSDRepScrub.h"
 #include "messages/MOSDScrubReserve.h"
-#include "common/EventTrace.h"
-
-#include "common/config.h"
-#include "include/compat.h"
 #include "mon/MonClient.h"
+#include "objclass/objclass.h"
+#include "osd/ClassHandler.h"
+#include "osd/OpRequest.h"
+#include "osd/Session.h"
 #include "osdc/Objecter.h"
+#include "scrubber/PrimaryLogScrub.h"
+
+// required includes order:
 #include "json_spirit/json_spirit_value.h"
 #include "json_spirit/json_spirit_reader.h"
 #include "include/ceph_assert.h"  // json_spirit clobbers it
@@ -12850,8 +12846,7 @@ void PrimaryLogPG::on_shutdown()
   }
 
   m_scrubber->scrub_clear_state();
-
-  m_scrubber->unreg_next_scrub();
+  m_scrubber->rm_from_osd_scrubbing();
 
   vector<ceph_tid_t> tids;
   cancel_copy_ops(false, &tids);
