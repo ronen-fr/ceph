b07bcddc4d7ae78670fccff2c27134b6106ec27f
Ronen Friedman <rfriedma@redhat.com>
2022-04-20 06:11:24 +0000

    Split from "test fixture fixes"

--- src/osd/scrubber/scrub_backend.cc
+++ src/osd/scrubber/scrub_backend.cc
@@ -872,12 +872,11 @@ std::optional<std::string> ScrubBackend::compare_obj_in_maps(
   auto [auths, objerrs] =
     match_in_shards(ho, auth_res, object_error, errstream);
 
-  auto opt_ers =
-    for_empty_auth_list(std::move(auths),
-                        std::move(objerrs),
-                        auth,
-                        ho,
-                        errstream);
+  auto opt_ers = for_empty_auth_list(std::move(auths),
+                                     std::move(objerrs),
+                                     auth,
+                                     ho,
+                                     errstream);
 
   if (opt_ers.has_value()) {
 
@@ -1287,7 +1286,9 @@ bool ScrubBackend::compare_obj_details(pg_shard_t auth_shard,
     auth_bl.push_back(auth_attr->second);
 
     if (!can_bl.contents_equal(auth_bl)) {
-      format_to(std::back_inserter(out), "{}object info inconsistent ", sep(error));
+      format_to(std::back_inserter(out),
+                "{}object info inconsistent ",
+                sep(error));
       obj_result.set_object_info_inconsistency();
     }
   }
@@ -1307,7 +1308,9 @@ bool ScrubBackend::compare_obj_details(pg_shard_t auth_shard,
       auth_bl.push_back(auth_attr->second);
 
       if (!can_bl.contents_equal(auth_bl)) {
-        format_to(std::back_inserter(out), "{}snapset inconsistent ", sep(error));
+        format_to(std::back_inserter(out),
+                  "{}snapset inconsistent ",
+                  sep(error));
         obj_result.set_snapset_inconsistency();
       }
     }
@@ -1386,10 +1389,16 @@ bool ScrubBackend::compare_obj_details(pg_shard_t auth_shard,
 
     auto cand = candidate.attrs.find(k);
     if (cand == candidate.attrs.end()) {
-      format_to(std::back_inserter(out), "{}attr name mismatch '{}'", sep(error), k);
+      format_to(std::back_inserter(out),
+                "{}attr name mismatch '{}'",
+                sep(error),
+                k);
       obj_result.set_attr_name_mismatch();
     } else if (cand->second.cmp(v)) {
-      format_to(std::back_inserter(out), "{}attr value mismatch '{}'", sep(error), k);
+      format_to(std::back_inserter(out),
+                "{}attr value mismatch '{}'",
+                sep(error),
+                k);
       obj_result.set_attr_value_mismatch();
     }
   }
@@ -1402,7 +1411,10 @@ bool ScrubBackend::compare_obj_details(pg_shard_t auth_shard,
 
     auto in_auth = auth.attrs.find(k);
     if (in_auth == auth.attrs.end()) {
-      format_to(std::back_inserter(out), "{}attr name mismatch '{}'", sep(error), k);
+      format_to(std::back_inserter(out),
+                "{}attr name mismatch '{}'",
+                sep(error),
+                k);
       obj_result.set_attr_name_mismatch();
     }
   }
