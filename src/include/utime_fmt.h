d5868a7695e851bd3e13293461f874a75e892c3a
Ronen Friedman <rfriedma@redhat.com>
2022-08-16 15:48:04 +0300

    osd/scrub: reduce the amount of log lines
    Following call chains, and removing repeated data.
    
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>

--- src/include/utime_fmt.h
+++ src/include/utime_fmt.h
@@ -4,29 +4,44 @@
 /**
  * \file fmtlib formatter for utime_t
  */
-#include <fmt/format.h>
 #include <fmt/chrono.h>
-
-#include <string_view>
+#include <fmt/format.h>
 
 #include "include/utime.h"
 
 template <>
 struct fmt::formatter<utime_t> {
-  constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
+  template <typename ParseContext>
+  constexpr auto parse(ParseContext& ctx)
+  {
+    auto it = ctx.begin();
+    if (it != ctx.end() && *it == 's') {
+      short_format = true;
+      ++it;
+    }
+    return it;
+  }
 
   template <typename FormatContext>
   auto format(const utime_t& utime, FormatContext& ctx)
   {
     if (utime.sec() < ((time_t)(60 * 60 * 24 * 365 * 10))) {
       // raw seconds.  this looks like a relative time.
-      return fmt::format_to(ctx.out(), "{}.{:06}", (long)utime.sec(),
-                            utime.usec());
+      return fmt::format_to(
+	ctx.out(), "{}.{:06}", (long)utime.sec(), utime.usec());
     }
 
     // this looks like an absolute time.
     // conform to http://en.wikipedia.org/wiki/ISO_8601
-    auto asgmt = fmt::gmtime(utime.sec());
-    return fmt::format_to(ctx.out(), "{:%FT%T}.{:06}{:%z}", asgmt, utime.usec(), asgmt);
+    // (unless short_format is set)
+    auto aslocal = fmt::localtime(utime.sec());
+    if (short_format) {
+      return fmt::format_to(
+	ctx.out(), "{:%FT%T}.{:03}", aslocal, utime.usec() / 1000);
+    }
+    return fmt::format_to(
+      ctx.out(), "{:%FT%T}.{:06}{:%z}", aslocal, utime.usec(), aslocal);
   }
+
+  bool short_format{false};
 };
