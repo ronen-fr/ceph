a608b7bc8457561ff97c47ecb0eb8b5bec92ab38
Ronen Friedman <rfriedma@redhat.com>
2023-09-11 01:56:33 -0500

    osd/scrub: complete ScrubJob transition from within ScrubQueue
    ScrubJob is now in the Scrub namespace.
    
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>

--- src/osd/scrubber/scrub_job.h
+++ src/osd/scrubber/scrub_job.h
@@ -1,5 +1,39 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
+#pragma once
+
+#include <atomic>
+#include <chrono>
+#include <memory>
+#include <vector>
+
+#include "common/RefCountedObj.h"
+#include "common/ceph_atomic.h"
+#include "include/utime_fmt.h"
+#include "osd/osd_types.h"
+#include "osd/osd_types_fmt.h"
+#include "osd/scrubber_common.h"
+
+
+/**
+ * The ID used to name a candidate to scrub:
+ * - in this version: a PG is identified by its spg_t
+ * - in the (near) future: a PG + a scrub type (shallow/deep)
+ */
+using ScrubTargetId = spg_t;
+
+
+namespace Scrub {
+
+enum class must_scrub_t { not_mandatory, mandatory };
+
+enum class qu_state_t {
+  not_registered,  // not a primary, thus not considered for scrubbing by this
+		   // OSD (also the temporary state when just created)
+  registered,	   // in either of the two queues ('to_scrub' or 'penalized')
+  unregistering	   // in the process of being unregistered. Will be finalized
+		   // under lock
+};
 
 struct scrub_schedule_t {
   utime_t scheduled_at{};
@@ -11,11 +45,11 @@ struct sched_params_t {
   utime_t proposed_time{};
   double min_interval{0.0};
   double max_interval{0.0};
-    must_scrub_t is_must{ScrubQueue::must_scrub_t::not_mandatory};
+  must_scrub_t is_must{must_scrub_t::not_mandatory};
 };
 
-  struct ScrubJob final : public RefCountedObject {
-
+class ScrubJob final : public RefCountedObject {
+ public:
   /**
    * a time scheduled for scrub, and a deadline: The scrub could be delayed
    * if system load is too high (but not if after the deadline),or if trying
@@ -64,6 +98,7 @@ struct sched_params_t {
 
   utime_t get_sched_time() const { return schedule.scheduled_at; }
 
+  static std::string_view qu_state_text(qu_state_t st);
 
   /**
    * relatively low-cost(*) access to the scrub job's state, to be used in
@@ -72,10 +107,10 @@ struct sched_params_t {
    */
   std::string_view state_desc() const
   {
-      return ScrubQueue::qu_state_text(state.load(std::memory_order_relaxed));
+    return qu_state_text(state.load(std::memory_order_relaxed));
   }
 
-    void update_schedule(const ScrubQueue::scrub_schedule_t& adjusted);
+  void update_schedule(const scrub_schedule_t& adjusted);
 
   void dump(ceph::Formatter* f) const;
 
@@ -101,6 +136,43 @@ struct sched_params_t {
    * are we already scheduled for a scrub/deep scrub? when?
    */
   std::string scheduling_state(utime_t now_is, bool is_deep_expected) const;
-
-  friend std::ostream& operator<<(std::ostream& out, const ScrubJob& pg);
 };
+
+using ScrubJobRef = ceph::ref_t<ScrubJob>;
+using ScrubQContainer = std::vector<ScrubJobRef>;
+}  // namespace Scrub
+
+std::ostream& operator<<(std::ostream& out, const Scrub::ScrubJob& pg);
+
+namespace fmt {
+template <>
+struct formatter<Scrub::qu_state_t> : formatter<std::string_view> {
+  template <typename FormatContext>
+  auto format(const Scrub::qu_state_t& s, FormatContext& ctx)
+  {
+    auto out = ctx.out();
+    out = fmt::formatter<string_view>::format(
+	std::string{Scrub::ScrubJob::qu_state_text(s)}, ctx);
+    return out;
+  }
+};
+
+template <>
+struct formatter<Scrub::ScrubJob> {
+  constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
+
+  template <typename FormatContext>
+  auto format(const Scrub::ScrubJob& sjob, FormatContext& ctx)
+  {
+    return fmt::format_to(
+	ctx.out(),
+	"pg[{}] @ {:s} (dl:{:s}) - <{}> / failure: {} / pen. t.o.: {:s} / "
+	"queue "
+	"state: {:.7}",
+	sjob.pgid, sjob.schedule.scheduled_at, sjob.schedule.deadline,
+	sjob.registration_state(), sjob.resources_failure, sjob.penalty_timeout,
+	sjob.state.load(std::memory_order_relaxed));
+  }
+};
+}  // namespace fmt
+
