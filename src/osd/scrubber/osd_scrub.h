40abff884f94a783dde4369436004d887a6d3b28
Ronen Friedman <rfriedma@redhat.com>
2023-09-15 09:03:09 -0500

    osd/scrub: move OSD::sched_scrub() to OsdScrub
    ... (as OsdScrub::initiate_scrub()).
    
    The random backoff dice roller (scrub_random_backoff())
    is moved as well.
    
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>

--- src/osd/scrubber/osd_scrub.h
+++ src/osd/scrubber/osd_scrub.h
@@ -35,6 +35,11 @@ class OsdScrub {
   std::ostream& gen_prefix(std::ostream& out, std::string_view fn) const;
 
   /**
+   *  select a target from the queue, and initiate a scrub
+   */
+  void initiate_scrub(bool active_recovery);
+
+  /**
    * logs a string at log level 20, using OsdScrub's prefix.
    * An aux function to be used by sub-objects.
    */
@@ -180,4 +185,11 @@ class OsdScrub {
 
   /// number of PGs stuck while scrubbing, waiting for objects
   int get_blocked_pgs_count() const;
+
+  /**
+   * roll a dice to determine whether we should skip this tick, not trying to
+   * schedule a new scrub.
+   * \returns true with probability of osd_scrub_backoff_ratio.
+   */
+  bool scrub_random_backoff() const;
 };
