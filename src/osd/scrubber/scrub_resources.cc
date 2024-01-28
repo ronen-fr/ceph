5af384d8c8e49b25f85263a7bd3e78d223a77faa
Ronen Friedman <rfriedma@redhat.com>
2024-01-28 05:02:31 -0600

    osd/scrub: remove remote reservation code from ScrubResources
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>

--- src/osd/scrubber/scrub_resources.cc
+++ src/osd/scrubber/scrub_resources.cc
@@ -69,49 +69,6 @@ void ScrubResources::dec_scrubs_local()
   ceph_assert(scrubs_local >= 0);
 }
 
-// ------------------------- scrubbing on this OSD as replicas ----------------
-
-bool ScrubResources::inc_scrubs_remote(pg_t pgid)
-{
-  std::lock_guard lck{resource_lock};
-
-  // if this PG is already reserved - it's probably a benign bug.
-  // report it, but do not fail the reservation.
-  if (granted_reservations.contains(pgid)) {
-    log_upwards(fmt::format("{}: pg[{}] already reserved", __func__, pgid));
-    return true;
-  }
-
-  auto pre_op_cnt = granted_reservations.size();
-  if (std::cmp_less(pre_op_cnt, conf->osd_max_scrubs)) {
-    granted_reservations.insert(pgid);
-    log_upwards(fmt::format(
-	"{}: pg[{}] reserved. Remote scrubs count changed from {} -> {} (max "
-	"{}, local {})",
-	__func__, pgid, pre_op_cnt, granted_reservations.size(),
-	conf->osd_max_scrubs, scrubs_local));
-    return true;
-  }
-
-  log_upwards(fmt::format(
-      "{}: pg[{}] failed. Too many concurrent replica scrubs ({} >= max ({}))",
-      __func__, pgid, pre_op_cnt, conf->osd_max_scrubs));
-  return false;
-}
-
-void ScrubResources::dec_scrubs_remote(pg_t pgid)
-{
-  std::lock_guard lck{resource_lock};
-  // we might not have this PG in the set (e.g. if we are concluding a
-  // high priority scrub, one that does not require reservations)
-  auto cnt = granted_reservations.erase(pgid);
-  if (cnt) {
-    log_upwards(fmt::format(
-	"{}: remote reservation for {} removed -> {} (max {}, local {})",
-	__func__, pgid, granted_reservations.size(), conf->osd_max_scrubs,
-	scrubs_local));
-  }
-}
 
 void ScrubResources::dump_scrub_reservations(ceph::Formatter* f) const
 {
