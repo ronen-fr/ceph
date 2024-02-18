777a55501c3b1335f6de34e54fb808908d8b580d
Ronen Friedman <rfriedma@redhat.com>
2024-02-18 00:32:10 -0600

    common: improve hobject_t fmtlib formatter performance
    The new version of the code only takes about 38% of the time of the old
    one.
    See https://github.com/ronen-fr/hobjtostr/tree/rf-2 for the code used
    to benchmark the new version.
    
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>

--- src/common/hobject_fmt.h
+++ src/common/hobject_fmt.h
@@ -5,31 +5,40 @@
 /**
  * \file fmtlib formatters for some hobject.h classes
  */
+#include <fmt/compile.h>
 #include <fmt/format.h>
 #include <fmt/ranges.h>
 
 #include "common/hobject.h"
 #include "msg/msg_fmt.h"
 
-// \todo reimplement
-static inline void append_out_escaped(const std::string& in, std::string* out)
-{
-  for (auto i = in.cbegin(); i != in.cend(); ++i) {
-    if (*i == '%' || *i == ':' || *i == '/' || *i < 32 || *i >= 127) {
-      char buf[4];
-      snprintf(buf, sizeof(buf), "%%%02x", (int)(unsigned char)*i);
-      out->append(buf);
-    } else {
-      out->push_back(*i);
-    }
-  }
-}
 
-template <> struct fmt::formatter<hobject_t> {
+namespace fmt {
+template <>
+struct formatter<hobject_t> {
+
+  template <typename FormatContext>
+  static inline auto
+  append_sanitized(FormatContext& ctx, const std::string& in, int sep = 0)
+  {
+    for (auto i = in.cbegin(); i != in.cend(); ++i) {
+      if (*i == '%' || *i == ':' || *i == '/' || *i < 32 || *i >= 127) {
+	//fmt::format_to(ctx.out(), FMT_COMPILE("%{:02x}"), (int)(unsigned char)*i);
+	fmt::format_to(ctx.out(), FMT_COMPILE("%{:02x}"), *i);
+      } else {
+	fmt::format_to(ctx.out(), FMT_COMPILE("{}"), *i);
+      }
+    }
+    if (sep) {
+      fmt::format_to(ctx.out(), FMT_COMPILE("{}"), char(sep));
+    }
+    return ctx.out();
+  }
 
   constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
 
-  template <typename FormatContext> auto format(const hobject_t& ho, FormatContext& ctx)
+  template <typename FormatContext>
+  auto format(const hobject_t& ho, FormatContext& ctx)
   {
     if (ho == hobject_t{}) {
       return fmt::format_to(ctx.out(), "MIN");
@@ -39,14 +48,13 @@ template <> struct fmt::formatter<hobject_t> {
       return fmt::format_to(ctx.out(), "MAX");
     }
 
-    std::string v;
-    append_out_escaped(ho.nspace, &v);
-    v.push_back(':');
-    append_out_escaped(ho.get_key(), &v);
-    v.push_back(':');
-    append_out_escaped(ho.oid.name, &v);
-
-    return fmt::format_to(ctx.out(), "{}:{:08x}:{}:{}", static_cast<uint64_t>(ho.pool),
-			  ho.get_bitwise_key_u32(), v, ho.snap);
+    fmt::format_to(
+	ctx.out(), FMT_COMPILE("{}:{:08x}:"), static_cast<uint64_t>(ho.pool),
+	ho.get_bitwise_key_u32());
+    append_sanitized(ctx, ho.nspace, ':');
+    append_sanitized(ctx, ho.get_key(), ':');
+    append_sanitized(ctx, ho.oid.name);
+    return fmt::format_to(ctx.out(), FMT_COMPILE(":{}"), ho.snap);
   }
 };
+}  // namespace fmt
